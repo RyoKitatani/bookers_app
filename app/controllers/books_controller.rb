09db56b534a57b0ea0493f8093ece0413df78e92
RyoKitatani <ryo0929m@gmail.com>
2021-02-02 00:33:17 +0000

    [Update]4-8 投稿機能ルーティング調整

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,12 +8,20 @@ class BooksController < ApplicationController
   end
 
   def create 
-    
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
