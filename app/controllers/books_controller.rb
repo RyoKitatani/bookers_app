f27745ac705e7415ea206066ed2f1715f2739e8a
RyoKitatani <ryo0929m@gmail.com>
2021-02-02 04:53:10 +0000

    [Finish]4-8 投稿一覧の表作成及び修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,7 +19,6 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
-  
   end
   
   private
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_01_233849) do
+ActiveRecord::Schema.define(version: 2021_02_02_041323) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
