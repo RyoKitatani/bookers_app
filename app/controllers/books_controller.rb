0c83449f6dd5bdc924759531b8e524ef7cf8f4ce
RyoKitatani <ryo0929m@gmail.com>
2021-02-02 06:20:39 +0000

    [Fix]4-7 Update機能の追加及び修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
   end
 
   def create 
-    book = Book.new(book_params)
+    book = Book.(book_params)
     book.save
     redirect_to book_path(book.id)
   end
@@ -19,6 +19,18 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+  
+  def update
+    book =Book.find(params[:id] )
+    book.update(book_params)
+    redirect_to book_path(book.id)
   end
   
   private
