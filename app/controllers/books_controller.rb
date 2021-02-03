348cb237a16f57cf6bf7cfde98cfeeca91a59d54
RyoKitatani <ryo0929m@gmail.com>
2021-02-03 12:28:49 +0000

    [Finish]4-8 削除機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,10 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all.order(created_at: :desc)
+    @book = Book.new
   end
-
-  def new
-    @book = Book.ne
-  end
-
-  def create 
+  
+  def create
     book = Book.new(book_params)
     if book.save
       flash[:notice] = "Book was successfully created."
@@ -26,15 +23,15 @@ class BooksController < ApplicationController
   end
   
   def destroy
-    book = Book.find(params[:id])
-    if book.destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
      flash[:notice] = "Book was successfully destroyed."
      redirect_to books_path
     end
   end
   
   def update
-    book =Book.find(params[:id] )
+    book =Book.find(params[:id])
     if book.update(book_params)
       flash[:notice] = "Book was successfully updated."
       redirect_to book_path(book.id)
@@ -43,6 +40,6 @@ class BooksController < ApplicationController
   
   private
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
