ee41082099800ca8ab34f5e119a37101f3dc79a3
RyoKitatani <ryo0929m@gmail.com>
2021-02-04 00:27:01 +0000

    [Fix]4-8 一覧表示の順番規則の修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all.order(created_at: :desc)
+    @books = Book.all.order(created_at: :asc)
     @book = Book.new
   end
 
