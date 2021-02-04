d69c04dbbfc7c9e67ce239df5d6634b4edcdfa2e
RyoKitatani <ryo0929m@gmail.com>
2021-02-04 06:38:47 +0000

    [Finish]4-8 RSpecテスト終了

--- db/migrate/20210201233849_create_books.rb
+++ db/migrate/20210201233849_create_books.rb
@@ -2,8 +2,7 @@ class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
       t.string :title, 
-      t.text :body, null: false
+      t.text :body,
       t.timestamps
     end
   end
-end
